f5639db38fb9368c724d61dd276325f7ff447d58
Yifei Zeng <yzeng33@my.bcit.ca>
2024-03-14 21:27:48 -0700

    Update intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -10,7 +10,7 @@ import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
     return (
-        <section>
+        <section className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
             <div className="flex items-center justify-center">
                 <div className="relative">
                     <motion.div
@@ -66,35 +66,40 @@ export default function Intro() {
                     delay: 0.1,
                 }}
             >
-                Contact me here{" "}
-                <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-
-
-                <a
-                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-                    href="/CV.pdf"
-                    download
+                <Link
+                    href="#contact"
+                    className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
                 >
-                    Download CV{" "}
-                    <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-                </a>
+                    Contact me here {" "}
+                    <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+                </Link>
 
-                <a
-                    className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                    href="https://linkedin.com"
-                    target="_blank"
-                >
-                    <BsLinkedin />
-                </a>
 
-                <a
-                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                    href="https://github.com"
-                    target="_blank"
-                >
-                    <FaGithubSquare />
-                </a>
-            </motion.div>
-        </section>
+            <a
+                className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                href="/YifeiZengCV.pdf"
+                download
+            >
+                Download CV{" "}
+                <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+            </a>
+
+            <a
+                className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://www.linkedin.com/in/yifei-zeng/"
+                target="_blank"
+            >
+                <BsLinkedin />
+            </a>
+
+            <a
+                className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://github.com/zengyifei327"
+                target="_blank"
+            >
+                <FaGithubSquare />
+            </a>
+        </motion.div>
+        </section >
     );
 }
