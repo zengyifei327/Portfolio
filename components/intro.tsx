562468e9fffb2880441597da2ff24201ba7d97a7
Yifei Zeng <yzeng33@my.bcit.ca>
2024-03-15 13:11:42 -0700

    Track intro active

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,10 +7,18 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
+    const { ref } = useSectionInView("Home", 0.5);
+    const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
     return (
-        <section className="mb-25 max-w-[55rem] text-center sm:mb-0 scroll-mt-[100rem]">
+        <section 
+            ref={ref}
+            id="home" 
+            className="mb-28 max-w-[55rem] text-center sm:mb-0 scroll-mt-[100rem]">
             <div className="flex items-center justify-center">
                 <div className="relative">
                     <motion.div
