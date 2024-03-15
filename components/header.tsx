11a8473a4c4b00a517fc1a78864933801b1fcc0e
Yifei Zeng <yzeng33@my.bcit.ca>
2024-03-15 12:54:36 -0700

    Track active section on header

--- components/header.tsx
+++ components/header.tsx
@@ -5,10 +5,11 @@ import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Header() {
-    const [activeSection, setActiveSection] =
-    useState("Home");
+    const { activeSection, setActiveSection, setTimeOfLastClick } =
+        useActiveSectionContext();
 
     return (
         <header className="z-[999] relative">
@@ -27,7 +28,7 @@ export default function Header() {
                             initial={{ y: -100, opacity: 0 }}
                             animate={{ y: 0, opacity: 1 }}
                         >
-                            <Link 
+                            <Link
                                 className={clsx(
                                     "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
                                     {
@@ -35,20 +36,23 @@ export default function Header() {
                                             activeSection === link.name,
                                     }
                                 )}
-                            href={link.hash}
-                            onClick={() => setActiveSection(link.name)}
+                                href={link.hash}
+                                onClick={() => {
+                                    setActiveSection(link.name);
+                                    setTimeOfLastClick(Date.now());
+                                }}
                             >{link.name}
 
                                 {link.name === activeSection && (
-                                <motion.span
-                                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
-                                    layoutId="activeSection"
-                                    transition={{
-                                        type: "spring",
-                                        stiffness: 380,
-                                        damping: 30,
-                                    }}
-                                ></motion.span>
+                                    <motion.span
+                                        className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                                        layoutId="activeSection"
+                                        transition={{
+                                            type: "spring",
+                                            stiffness: 380,
+                                            damping: 30,
+                                        }}
+                                    ></motion.span>
                                 )}
                             </Link>
                         </motion.li>
